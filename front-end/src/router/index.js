1520ad3232343126ce3c20aea080618231042ea3
romain hippeau <rominch.1993@gmail.com>
2020-11-08 19:29:06 +0100

    suite a une erreur impossible a résoudre ,relancement du projet vue js avec les fichiers précédements utilisée

---
+++ front-end/src/router/index.js
@@ -0,0 +1,28 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from '../views/Home.vue';
+import Login from '../views/login'
+import Register from '../views/register';
+
+Vue.use(Router);
+
+export const router = new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/home',
+      component: Home
+    },
+    {
+      path: '/login',
+      component: Login
+    },
+    {
+      path: '/register',
+      component: Register
+    }]})
