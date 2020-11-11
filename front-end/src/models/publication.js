989dffeafb569460f304396cf5bb23a7acd909e1
romain hippeau <rominch.1993@gmail.com>
2020-11-11 04:12:04 +0100

    mise en place du model pour les publications

---
+++ front-end/src/models/publication.js
@@ -0,0 +1,9 @@
+export default class Publication {
+    constructor(title, attachment, createdAt, firstname, lastname) {
+        this.title = title;
+        this.attachment = attachment;
+        this.createdAt = createdAt;
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+}
