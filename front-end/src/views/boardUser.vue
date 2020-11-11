c8a76cd85efffb19a49d825430aa1054fb3b56ad
romain hippeau <rominch.1993@gmail.com>
2020-11-11 18:48:32 +0100

    mise en place du template et style pour publier une publication

---
+++ front-end/src/views/boardUser.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="container">
+    <header class="jumbotron">
+      <button @click="goToPublish" class="btn btn-primary btn-block button">Créer une publication</button>
+      <Publication v-for="publication in publications" :key="publication.id" :publication="publication" @infosPublication="setDataInPublication"/>
+    </header>
+    
+  </div>
+</template>
+
+<script>
+import {mapState} from 'vuex';
+import Publication from '../components/publi';
+
+
+export default {
+  name: 'User',
+  components: {
+    Publication
+  },
+  data(){
+    return {
+      publication:{
+        id: "",
+        title: "",
+        attachment:"",
+        userId:"",
+        createdAt:"",
+      }
+    }
+  },
+  computed: {
+    ...mapState([
+      'publications'
+    ])
+  },
+  methods: {
+    setDataInPublication(payload){
+      this.publication = payload.publication;
+    },
+    goToPublish(){
+      this.$router.push('/publication');
+    }
+  },
+  mounted(){
+    this.$store.dispatch('loadPublications')
+  }
+};
+
+</script>
+
+
+<style lang="scss" scoped>
+body{
+  background-color: lightgray;
+}
+.container{
+  display: flex;
+  justify-content: center;
+  border: none;
+}
+.jumbotron{
+  background-color: lightgray;
+  max-width: 1200px;
+  border: none;
+}
+.button{
+  background-color: #e70435;
+  border: none;
+  transition: all .5s;
+  transition-timing-function: cubic-bezier(.2, 3, .4, 1);
+  margin-bottom: 20px;
+}
+</style>
